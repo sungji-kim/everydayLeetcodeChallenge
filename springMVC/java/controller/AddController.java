21633ef86bf2dcd7b72bc798acd37728f62f8126
sungji-kim <54879344+sungji-kim@users.noreply.github.com>
2020-07-26 14:09:44 -0400

    Update AddController.java

--- springMVC/java/controller/AddController.java
+++ springMVC/java/controller/AddController.java
@@ -5,6 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+/*
+1. add @Controller annotation
+2. create a controller method
+- Create ModelAndView(mv)
+- mv.setViewName("display"); //can be name of .jsp file.
+- mv.addObject("name", object): send this object to the view.
+- parameters: e.g. @RequestParam("name") String name
+- return ModelAndView
+
+3. add @RequestMapping() annotation to the method 
+*/
+
+
 @Controller
 public class AddController {
 	
