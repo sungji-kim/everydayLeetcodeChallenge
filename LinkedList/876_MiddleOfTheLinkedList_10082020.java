66a2bde12dff710b4746fbf96b64caa0693599c1
sungji-kim <54879344+sungji-kim@users.noreply.github.com>
2020-10-08 11:16:56 -0400

    Create 876_MiddleOfTheLinkedList_10082020.java

---
+++ LinkedList/876_MiddleOfTheLinkedList_10082020.java
@@ -0,0 +1,34 @@
+/**
+* 876. Middle of the Linked List
+* Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+*/
+
+/**
+* Fast&slow pointers.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
+    }
+}
