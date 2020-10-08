347d4d27d6f3ecebc46c5622dcc5d593797eea5f
sungji-kim <54879344+sungji-kim@users.noreply.github.com>
2020-10-08 10:52:46 -0400

    Create 83_RemoveDuplicatesFromSortedList_10082020.java

---
+++ LinkedList/83_RemoveDuplicatesFromSortedList_10082020.java
@@ -0,0 +1,34 @@
+/**
+* 83. Remove Duplicates from Sorted List
+* Given a sorted linked list, delete all duplicates such that each element appear only once.
+*/
+
+/**
+* Simple LinkedList Management. 
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode res = head;
+        
+        while(head != null && head.next != null){
+            if(head.val == head.next.val){
+                head.next = head.next.next;
+            } else{
+                head = head.next;            
+            }
+        }
+        
+        return res;
+        
+    }
+}
